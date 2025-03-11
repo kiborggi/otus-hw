400baead969062ccea223ac9df2233d49d807943
Малафеев Владислав <v.malafeev@csat.ru>
2025-03-11 23:03:32 +0300

    Add fat JAR build and find intersections of two random sets using Guava

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 group = "ru.otus.hw"
@@ -14,6 +15,26 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
+
+tasks {
+    build{
+        dependsOn(subprojects.map { it.tasks.named("shadowJar") })
+    }
+}
+
 tasks.test {
     useJUnitPlatform()
+}
+
+subprojects {
+    apply(plugin = "java") // Подключаем плагин Java во всех дочерних модулях
+
+    repositories {
+        mavenCentral()
+    }
+
+    dependencies {
+        // Используем версию Guava из gradle.properties
+        implementation("com.google.guava:guava:${project.findProperty("guava")}")
+    }
 }
